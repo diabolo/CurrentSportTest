eb27e73eeec2a99a8cfe37404aaca0c737a90a9e
Andrew Premdas <apremdas@yahoo.co.uk>
2010-11-11 18:33:54 +0000

    working navigation to team homepage

--- features/step_definitions/sport_steps.rb
+++ features/step_definitions/sport_steps.rb
@@ -2,6 +2,28 @@ When /^I visit the homepage$/ do
   visit '/'
 end
 
+When /^I follow the football navigation$/ do 
+  debugger
+  within(selector) do
+    click_link(link)
+  end
+  debugger
+  1
+end
+
 Then /^I should see the homepage$/ do
   page.has_selector?('title',  :text => 'BBC SPORT')
+end
+
+
+When /^I navigate from the homepage to the "([^"]*)" team page$/ do |team|
+  visit '/'
+  page.find(:css, "a.lhsnl", :text => "Football").click
+  page.find(:css, "a.lhsnl", :text => "My Club").click
+  page.find(:css, "a.lp", :text => team.upcase).click
+
+end
+
+Then /^I should be on the "([^"]*)" team page$/ do |team|
+  page.find(:css, ".sectionHeader", :text => team).should be_true
 end
---
+++ features/team_navigation.feature
@@ -0,0 +1,9 @@
+Feature: Team navigation
+  As a fan
+  I want to see news about my team
+  So I don't have to read about other rubbish teams
+  
+  Scenario: Go to team page from home page
+    When I navigate from the homepage to the "Arsenal" team page
+    Then I should be on the "Arsenal" team page
+    
